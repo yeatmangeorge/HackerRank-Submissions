6ad37797858aa9fddbf0480b43693e9ba13a31ca
yeatmangeorge <70824909+yeatmangeorge@users.noreply.github.com>
2021-12-03 01:08:37 +0000

    Completed Time_Conversion

--- Time_Conversion.cpp
+++ Time_Conversion.cpp
@@ -1,7 +1,7 @@
 #include <string>
 #include <iostream>
 
-std::string convertToDoubleDigitStrings(int a)
+std::string convertToDoubleDigitStrings(int& a)
 {
     if (a > 9)
     {
@@ -13,7 +13,7 @@ std::string convertToDoubleDigitStrings(int a)
     }
 }
 
-int convertToInt(char firstIndex, char secondIndex, std::string &s)
+int convertToInt(char firstIndex, char secondIndex, std::string& s)
 {
     std::string toIntString;
     toIntString.push_back(s[firstIndex]);
@@ -24,24 +24,29 @@ int convertToInt(char firstIndex, char secondIndex, std::string &s)
 
 std::string timeConversion(std::string s) {
     //Get hour, min and secs in int form
-    int hour = convertToInt(0, 1,s);
-    int min = convertToInt(3, 4,s);
-    int sec = convertToInt(6, 7,s);
+    int hour = convertToInt(0, 1, s);
+    int min = convertToInt(3, 4, s);
+    int sec = convertToInt(6, 7, s);
 
     //Convert from 12 to 24 hour time
     bool pm = (s[8] == 'P') ? true : false;
-    hour = (pm) ? hour +12 : hour;
+    hour = (pm) ? hour + 12 : hour;
+
+    //Exceptions
+    hour = (!pm && hour == 12) ? 0 : hour;
+    hour = (pm && hour == 24) ? 12 : hour;
 
     //Build the output string in correct format
-    std::string output=
+    std::string output =
         convertToDoubleDigitStrings(hour)
-        +":"
-        +convertToDoubleDigitStrings(min)
-        +":"
-        +convertToDoubleDigitStrings(sec);
+        + ":"
+        + convertToDoubleDigitStrings(min)
+        + ":"
+        + convertToDoubleDigitStrings(sec);
 
     return output;
 }
+
 
 void main()
 {
